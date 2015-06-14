fbd857e92785cf909900f89c5d195d824e292508
Peter Ching <peter.ching@gmail.com>
2015-06-14 17:53:25 -0400

    Extend Backbone.View with basic Item view.

---
+++ js/views/itemView.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.itemView = Backbone.View.extend({
+
+	// which html tag each item should be place in, optional
+	tagName: "article",
+
+	// CSS classname, optional
+	className: "item"
+
+});
